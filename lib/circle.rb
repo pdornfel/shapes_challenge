67f98663148c340758bfd40c7869b30b1558d830
Paul Dornfeld <pdornfel@gmail.com>
2013-12-08 00:18:30 -0500

    added specs for circle and two methods for circle - area and perimeter.

--- lib/circle.rb
+++ lib/circle.rb
@@ -1,1 +1,17 @@
-Circle
+class Circle
+
+  def initialize(radius)
+    @radius = radius
+    @pie = Math::PI
+  end
+
+  def area
+    (@pie * @radius **2).round(2)
+  end
+
+  def perimter
+    (2 * @pie * @radius).round(2)
+  end
+
+
+end
--- spec/circle_spec.rb
+++ spec/circle_spec.rb
@@ -1,1 +1,23 @@
-circle spec
+require_relative '../lib/circle'
+require 'rspec'
+
+describe Circle do
+
+  it 'calculates the area of a circle' do
+    circle = Circle.new(3)
+    expect(circle.area).to eql(28.27)
+
+  end
+
+  it 'calculates the perimeter of a circle' do
+    circle = Circle.new(3)
+    expect(circle.perimter).to eql(18.85)
+
+  end
+
+
+
+
+
+
+end
